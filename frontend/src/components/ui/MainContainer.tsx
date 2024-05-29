1c12c5653e1f09be3d76221b4e7d7dfc8ae331c1
Watson Lewis-Rodriguez <38282105+5eh@users.noreply.github.com>
2024-05-29 13:45:49 -0500

    :zap: (./Frontend) Main Container :zap:

--- frontend/src/components/ui/MainContainer.tsx
+++ frontend/src/components/ui/MainContainer.tsx
@@ -3,15 +3,22 @@ import { ChevronRightIcon } from "@heroicons/react/20/solid";
 export default function MainContainer() {
   return (
     <div className="relative isolate overflow-hidden bg-black">
-      <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
-        <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
+      <div className="absolute inset-0 -z-10">
+        <img
+          src="https://i.ibb.co/mv7t9GL/Background.png"
+          alt="App screenshot"
+          className="h-full w-full object-cover opacity-10"
+        />
+      </div>
+      <div className="relative px-6 sm:pb-32 lg:flex lg:px-8">
+        <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl">
           <div className="mt-24 sm:mt-32 lg:mt-16">
             <a href="#" className="inline-flex space-x-6">
               <span className="rounded-full bg-indigo-500/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-400 ring-1 ring-inset ring-indigo-500/20">
-                What's new
+                FEATURED
               </span>
               <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-300">
-                <span>Just shipped v1.0</span>
+                <span>Collection v1.0</span>
                 <ChevronRightIcon
                   className="h-5 w-5 text-gray-500"
                   aria-hidden="true"
@@ -23,34 +30,32 @@ export default function MainContainer() {
             Karate Combat Collection
           </h1>
           <p className="mt-6 text-lg leading-8 text-gray-300">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-            fugiat aliqua.
+            This project management dashboard template is designed for project
+            managers
           </p>
           <div className="mt-10 flex items-center gap-x-6">
             <a
               href="#"
-              className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+              className="rounded-md border border-blue-400 hover:border-blue-500 bg-white/10 px-3.5 py-2.5 text-sm font-semibold text-white hover:bg-white/30"
             >
-              Get started
-            </a>
-            <a href="#" className="text-sm font-semibold leading-6 text-white">
-              Learn more <span aria-hidden="true">→</span>
+              Customize and Buy
             </a>
           </div>
         </div>
-        <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
-          <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
-            <img
-              src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
-              alt="App screenshot"
-              width={2432}
-              height={1442}
-              className="w-[76rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
-            />
-          </div>
-        </div>
-      </div>
+      </div>{" "}
+      <style jsx>{`
+        div::after {
+          content: "";
+          display: block;
+          width: calc(100% - 60px);
+          height: 1px;
+          background-color: #505050; /* Tailwind CSS red-300 */
+          position: absolute;
+          bottom: 0;
+          left: 30px;
+          right: 30px;
+        }
+      `}</style>
     </div>
   );
 }
