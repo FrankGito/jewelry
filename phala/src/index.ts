ac3309bfe246b145dedc38e38b043ea7ff8859d3
Frank Dierolf <frank_dierolf@web.de>
2024-05-31 17:17:13 +0200

    :zap: (phala) remove axios, delete dependencies

--- phala/src/index.ts
+++ phala/src/index.ts
@@ -1,11 +1,10 @@
 import { Request, Response, route } from "./httpSupport";
 import { renderHtml } from "./uiSupport";
-import axios from "axios";
 
 async function GET(req: Request): Promise<Response> {
   // Hardcode ApiKey && construct Header
   const api_key = "msy_71TlZfQPhg5RgkpwHIGeoXOsPEM36opN35Tx";
-  const headers = { Authorization: `Bearer ${api_key}` };
+  const headers = new Headers({ Authorization: `Bearer ${api_key}` });
 
   // Declare upper scoped  taskId & modelUrl variable
   let taskId = "";
@@ -13,7 +12,6 @@ async function GET(req: Request): Promise<Response> {
 
   // Declare prompt input
   const prompt = "Penguin";
-
 
   // Prepare API Payload
   const payload = {
@@ -29,30 +27,35 @@ async function GET(req: Request): Promise<Response> {
     /**https://docs.meshy.ai/api-text-to-3d-beta**/
     /*********************************************/
 
-    const response = await axios.post(
-      "https://api.meshy.ai/v2/text-to-3d",
-      payload,
-      { headers },
-    );
-    taskId = response.data.result;
+    const response = await fetch("https://api.meshy.ai/v2/text-to-3d", {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(payload)
+    });
 
-    const response2 = await axios.get(
-      `https://api.meshy.ai/v2/text-to-3d/${taskId}`,
-      { headers },
-    );
-    let status = response2.data.status;
+    const data = await response.json();
+    taskId = data.result;
+
+    const response2 = await fetch(`https://api.meshy.ai/v2/text-to-3d/${taskId}`, {
+      method: 'GET',
+      headers
+    });
+
+    const data2 = await response2.json();
+    let status = data2.status;
 
     // Loop thruh waittime. If status is finsihed, get the glb file.
     while (status === "IN_PROGRESS") {
-      const response = await axios.get(
-        `https://api.meshy.ai/v2/text-to-3d/${taskId}`,
-        { headers },
-      );
+      const response = await fetch(`https://api.meshy.ai/v2/text-to-3d/${taskId}`, {
+        method: 'GET',
+        headers
+      });
 
-      status = response.data.status;
+      const data = await response.json();
+      status = data.status;
 
       if (status === "SUCCEEDED") {
-        const { model_urls, thumbnail_url } = response.data;
+        const { model_urls, thumbnail_url } = data;
         if (model_urls && model_urls.glb) {
           modelUrl = model_urls.glb;
           // If Debug, do uncomment
