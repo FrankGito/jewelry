3a74d8184838e548a3b464fa785425ed4ce8b482
AemulaDev <dt@aemu.la>
2024-05-31 00:46:54 -0500

    :zap: (frontend) Three render polishing

--- frontend/src/components/canvas/Three.tsx
+++ frontend/src/components/canvas/Three.tsx
@@ -1,6 +1,8 @@
 import { Canvas, useLoader } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import * as THREE from 'three';
+import { OrbitControls, Environment, Float, ContactShadows } from '@react-three/drei';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { LayerMaterial, Depth, Noise } from 'lamina';
 
 interface ModelProps {
   url: string;
@@ -11,17 +13,44 @@ function Model({ url }: ModelProps) {
   return <primitive object={gltf.scene} />;
 }
 
-function Three() {
+export default function Three() {
   return (
-    <div id="canvas-container" style={{ width: '100vw', height: '100vh' }}>
-      <Canvas>
-        <ambientLight intensity={0.1} />
-        <directionalLight position={[0, 0, 5]} />
-        <Model url="/flamingo.glb" />
-        <OrbitControls />
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Canvas dpr={[1, 2]} camera={{ position: [0, 1, 5] }}>
+        <OrbitControls autoRotate minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 1.8} />
+        <pointLight position={[10, 10, 5]} />
+        <pointLight position={[-10, -10, -5]} />
+        <ambientLight intensity={0.4} />
+        <group position={[0, -1.5, 0]}>
+          <Float position={[0, 1, 0]} speed={0.1} rotationIntensity={0.1} floatIntensity={2}>
+            <mesh castShadow receiveShadow>
+              <Model url="/flamingo.glb" />
+              <meshStandardMaterial color="pink" roughness={0.1} metalness={0.925} />
+            </mesh>
+          </Float>
+          <ContactShadows scale={10} blur={3} opacity={0.25} far={10} />
+        </group>
+        <Environment background resolution={64}>
+          <Striplight position={[10, 2, 0]} scale={[1, 1, 20]} />
+          <Striplight position={[-10, 2, 0]} scale={[1, 1, 20]} />
+          <mesh scale={100}>
+            <sphereGeometry args={[1, 64, 64]} />
+            <LayerMaterial side={THREE.BackSide}>
+              <Depth colorA="#00ffff" colorB="#ff8f00" alpha={0.5} mode="normal" near={0} far={300} origin={[100, 100, 100]} />
+              <Noise mapping="local" type="cell" scale={0.5} mode="softlight" />
+            </LayerMaterial>
+          </mesh>
+        </Environment>
       </Canvas>
     </div>
-  );
+  )
 }
 
-export default Three;
+function Striplight(props: JSX.IntrinsicElements['mesh']) {
+  return (
+    <mesh {...props}>
+      <boxGeometry />
+      <meshBasicMaterial color="white" />
+    </mesh>
+  )
+}
